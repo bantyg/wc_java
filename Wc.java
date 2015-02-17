36092bca3b8765479f111f0062c8fdbdc1db9f2b
Pooja <poojag@thoughtworks.com>
2015-02-17 14:48:10 +0530

    countLines will count number of lines present in the string

--- Wc.java
+++ Wc.java
@@ -1,4 +1,10 @@
 class Wc{
+	public int countLines(String str){
+		String[] splited = str.split("\r\n");
+		return splited.length;
+
+	}
+
 	public int countWords(String str){
 		String[] splited = str.split(" ");
 		return splited.length;
--- WcTest.java
+++ WcTest.java
@@ -1,6 +1,20 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class WcTest{
+
+	@Test
+	public void countLines_will_counts_number_of_lines_of_string_which_has_3_lines(){
+		Wc wc = new Wc();
+		assertEquals(3,wc.countLines("hello how are you.\r\n I am fine \r\n what about you"));
+	}
+
+	@Test
+	public void countLines_will_counts_number_of_lines_of_string_which_has_4_lines(){
+		Wc wc = new Wc();
+		assertEquals(4,wc.countLines("hello how are you.\r\n I am fine \r\n what about you \r\n Bawlt"));
+	}
+
+
 	@Test
 	public void countWord_will_counts_number_of_words_of_string(){
 		Wc wc = new Wc();
